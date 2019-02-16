e201d49acdd3e21866773fcd2b9c5df025e43388
sjncube <micanjohnscoderx@gmail.com>
2019-02-16 08:51:25 +0200

    second commit

--- pipeline_sensor.py
+++ pipeline_sensor.py
@@ -1,17 +1,25 @@
-# Python Summative
+# Python Sumnative Assessment
 # author Sizwe Ncube
+import random
+import datetime
 
+#function for Problem:1 and Problem:2
 def Funct_Cluster():
-    """ create 32 clusters and 16 transducer
+    """
+        create 32 clusters and 16 transducer
         sensors in each and every cluster
     """
-    import random
     cluster = {}
     for cluster_counter in range(1, 33):
         sensor = []
         for sensor_counter in range(1, 17):
-            randNum = random.uniform(0, 1)
-            sensor.append(randNum)
+            randnum = random.uniform(0, 1)
+            sensor.append(randnum)
         cluster[cluster_counter] = sensor
-    return cluster
-#print(Funct_Cluster())
+    file = open("sensor_data.txt", "a")
+    file.write(str(datetime.datetime.now())+"\n" + 1024*"-" + "\n")
+    file.write(str(cluster)+"\n" + 1024*"-" + "\n")
+    file.close()
+
+
+Funct_Cluster()
